c4597462b09b86e14fba0e12ea7308fbcbd493b3
aeonic <ericknwash@gmail.com>
2022-02-11 11:43:27 -0600

    Move mob bucket settings to common config

--- src/main/java/design/aeonic/iota/event/setup/DispenserBehaviors.java
+++ src/main/java/design/aeonic/iota/event/setup/DispenserBehaviors.java
@@ -16,45 +16,39 @@ import net.minecraft.tags.FluidTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.animal.Bucketable;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.SolidBucketItem;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.block.LavaCauldronBlock;
 import net.minecraft.world.level.block.LayeredCauldronBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.DispenserBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.logging.log4j.LogManager;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.function.Supplier;
 
 public class DispenserBehaviors {
+
     public static final DefaultDispenseItemBehavior DEFAULT_EMPTY_BUCKET_BEHAVIOR = (DefaultDispenseItemBehavior) DispenserBlock.DISPENSER_REGISTRY.get(Items.BUCKET);
     public static final DefaultDispenseItemBehavior DEFAULT_FULL_BUCKET_BEHAVIOR = (DefaultDispenseItemBehavior) DispenserBlock.DISPENSER_REGISTRY.get(Items.WATER_BUCKET);
 
     public static void register() {
-        if (Iota.commonConfig.dispenserCanEmptyCauldron().get())
-            DispenserBlock.registerBehavior(Items.BUCKET, EmptyBucketBehavior.INSTANCE);
+        DispenserBlock.registerBehavior(Items.BUCKET, EmptyBucketBehavior.INSTANCE);
+        DispenserBlock.registerBehavior(Items.WATER_BUCKET, FullBucketBehavior.INSTANCE);
+        DispenserBlock.registerBehavior(Items.LAVA_BUCKET, FullBucketBehavior.INSTANCE);
+        DispenserBlock.registerBehavior(Items.POWDER_SNOW_BUCKET, FullBucketBehavior.INSTANCE);
 
-        if (Iota.commonConfig.dispenserCanFillWaterCauldron().get())
-            DispenserBlock.registerBehavior(Items.WATER_BUCKET, FullBucketBehavior.INSTANCE);
-
-        if (Iota.commonConfig.dispenserCanFillLavaCauldron().get())
-            DispenserBlock.registerBehavior(Items.LAVA_BUCKET, FullBucketBehavior.INSTANCE);
-
-        if (Iota.commonConfig.dispenserCanFillPowderSnowCauldron().get())
-            DispenserBlock.registerBehavior(Items.POWDER_SNOW_BUCKET, FullBucketBehavior.INSTANCE);
-
-        if (Iota.commonConfig.dispenserCanFillAnimalCauldron().get()) {
-            List<Item> mobBucketItems = IotaTags.Items.BUCKET_HAS_MOB.getValues();
-            for (var item: mobBucketItems) {
-                DispenserBlock.registerBehavior(item, FullBucketBehavior.INSTANCE);
-            }
+        List<Item> mobBucketItems = Iota.commonConfig.mobBucketItems().get().getItems();
+        for (var item: mobBucketItems) {
+            DispenserBlock.registerBehavior(item, FullBucketBehavior.INSTANCE);
         }
     }
 
@@ -66,8 +60,10 @@ public class DispenserBehaviors {
         @Nonnull
         @Override
         public ItemStack execute(BlockSource source, @Nonnull ItemStack stack) {
-            // TODO: find a mod-agnostic way to implement the rest of this.
+            // TODO: find a mod-agnostic way to implement the rest of this
             //  Figure out what mods make changes to cauldron etc
+
+            if (!(Iota.serverConfig.dispenserCanEmptyCauldron().get())) return DEFAULT_FULL_BUCKET_BEHAVIOR.dispense(source, stack);
 
             ServerLevel levelaccessor = source.getLevel();
             BlockPos blockpos = source.getPos().relative(source.getBlockState().getValue(DispenserBlock.FACING));
@@ -77,25 +73,28 @@ public class DispenserBehaviors {
 
             if (!state.is(Blocks.CAULDRON) || !(stack.getItem() instanceof BucketItemAccess item)) return DEFAULT_FULL_BUCKET_BEHAVIOR.dispense(source, stack);
 
-            if (item.getContent().is(FluidTags.WATER)) {
+            if (Iota.serverConfig.dispenserCanFillWaterCauldron().get() && item.getContent().is(FluidTags.WATER)) {
                 newState = Blocks.WATER_CAULDRON.defaultBlockState().setValue(LayeredCauldronBlock.LEVEL, 3);
-            } else if (item.getContent().is(FluidTags.LAVA)) {
+            } else if (Iota.serverConfig.dispenserCanFillLavaCauldron().get() && item.getContent().is(FluidTags.LAVA)) {
                 newState = Blocks.LAVA_CAULDRON.defaultBlockState();
-            } else if (item instanceof SolidBucketItem solidBucketItem && solidBucketItem.getBlock() == Blocks.POWDER_SNOW) {
+            } else if (Iota.serverConfig.dispenserCanFillPowderSnowCauldron().get() &&
+                    item instanceof SolidBucketItem solidBucketItem && solidBucketItem.getBlock() == Blocks.POWDER_SNOW) {
                 newState = (Blocks.POWDER_SNOW_CAULDRON).defaultBlockState().setValue(LayeredCauldronBlock.LEVEL, 3);
             }
 
             if (newState != null) {
                 levelaccessor.setBlock(blockpos, newState, 2);
 
+                if (item instanceof MobBucketItemAccess mobBucketItem) {
+                    LogManager.getLogger().info("aaa {}", mobBucketItem);
+                    if (!Iota.serverConfig.dispenserCanFillAnimalCauldron().get()) { return DEFAULT_FULL_BUCKET_BEHAVIOR.dispense(source, stack); }
+                    mobBucketItem.callSpawn(levelaccessor, stack, blockpos);
+                }
+
                 if (item instanceof SolidBucketItem bi)
                     levelaccessor.playSound(null, blockpos, bi.getBlock().getSoundType(bi.getBlock().defaultBlockState()).getPlaceSound(),
                             SoundSource.BLOCKS, 1f, 1f);
                 else item.callPlayEmptySound(null, levelaccessor, blockpos);
-
-                if (item instanceof MobBucketItemAccess mobBucketItem) {
-                    mobBucketItem.callSpawn(levelaccessor, stack, blockpos);
-                }
 
                 stack.shrink(1);
                 if (stack.isEmpty()) {
@@ -134,7 +133,7 @@ public class DispenserBehaviors {
                         AABB.ofSize(Vec3.atCenterOf(new Vec3i(source.x(), source.y(), source.z())), 2d, 2d, 2d), // slightly bigger than one block
                         EntitySelector.LIVING_ENTITY_STILL_ALIVE.and(e -> e instanceof Bucketable));
 
-                if ((Iota.commonConfig.dispenserCanEmptyAnimalCauldron().get()) && entities.get().size() > 0) {
+                if ((Iota.serverConfig.dispenserCanEmptyAnimalCauldron().get()) && entities.get().size() > 0) {
                     var entity = entities.get().get(0);
                     var entityBucketable = ((Bucketable) entity);
                     var newStack = entityBucketable.getBucketItemStack();
--- src/main/java/design/aeonic/iota/registry/IotaTags.java
+++ src/main/java/design/aeonic/iota/registry/IotaTags.java
@@ -6,12 +6,13 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.fml.common.Mod;
 
 public final class IotaTags {
     public static final class Blocks {
         public static final Tag.Named<Block> NETHER_PORTAL_FRAME = BlockTags.bind(Iota.MOD_ID + ":nether_portal_frame");
     }
     public static final class Items {
-        public static final Tag.Named<Item> BUCKET_HAS_MOB = ItemTags.bind(Iota.MOD_ID + ":bucket_has_mob");
+
     }
 }
