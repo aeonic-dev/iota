d1b802c202482711f1e3531d90e6fb5bd2ca6af4
aeonic <ericknwash@gmail.com>
2022-02-11 02:06:27 -0600

    Oops, fix the tag name

--- src/main/java/design/aeonic/iota/registry/IotaTags.java
+++ src/main/java/design/aeonic/iota/registry/IotaTags.java
@@ -12,6 +12,6 @@ public final class IotaTags {
         public static final Tag.Named<Block> NETHER_PORTAL_FRAME = BlockTags.bind(Iota.MOD_ID + ":nether_portal_frame");
     }
     public static final class Items {
-        public static final Tag.Named<Item> BUCKET_HAS_MOB = ItemTags.bind(Iota.MOD_ID + ":bucket_has_block");
+        public static final Tag.Named<Item> BUCKET_HAS_MOB = ItemTags.bind(Iota.MOD_ID + ":bucket_has_mob");
     }
 }
